5dc80574ab660fb4c232fa437230c5ffdd493baa
Tejas Nasre <tejasnasre120@gmail.com>
2025-01-10 12:48:50 +0530

    added::rsvp

--- app/[eventid].tsx
+++ app/[eventid].tsx
@@ -6,11 +6,15 @@ import Entypo from '@expo/vector-icons/Entypo';
 import dayjs from 'dayjs';
 import { supabase } from '~/utils/supabase';
 import { eventlist } from '~/types/types';
+import { useAuth } from '~/contexts/AuthProvider';
 
 const eventPage = () => {
   const { eventid } = useLocalSearchParams();
 
+  const { user }: any = useAuth();
+
   const [event, setEvent] = useState<eventlist[] | null>([]);
+  const [attend, setAttend] = useState<{ user_id: any; event_id: any } | null>(null);
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
@@ -18,10 +22,29 @@ const eventPage = () => {
       setLoading(true);
       let { data: events, error } = await supabase.from('events').select('*').eq('id', eventid);
       setEvent(events);
+
+      const { data: attendeData } = await supabase
+        .from('attendance')
+        .select('user_id,event_id')
+        .eq('user_id,', user.id)
+        .eq('event_id', eventid)
+        .single();
+
+      setAttend(attendeData);
       setLoading(false);
     };
     fetcheventid();
   }, [eventid]);
+
+  const joinevent = async () => {
+    const { data, error } = await supabase
+      .from('attendance')
+      .insert({ user_id: user.id, event_id: eventid })
+      .select()
+      .single();
+
+    setAttend(data);
+  };
 
   if (loading) {
     return <ActivityIndicator />;
@@ -62,9 +85,18 @@ const eventPage = () => {
       {/* Footer */}
       <View className="border-t-1 absolute bottom-0 left-0 right-0 flex-row items-center justify-between border border-gray-300 px-6 py-8">
         <Text className="font-SpaceGrotesk text-xl">Free</Text>
-        <Pressable className="h-14 w-48 items-center justify-center rounded-xl bg-red-400 px-3">
-          <Text className="font-SpaceGrotesk text-xl text-white">Join And RSVP</Text>
-        </Pressable>
+
+        {attend ? (
+          <>
+            <Text className="font-SpaceGrotesk text-xl text-black">You Are Attending</Text>
+          </>
+        ) : (
+          <Pressable
+            onPress={() => joinevent()}
+            className="h-14 w-48 items-center justify-center rounded-xl bg-red-400 px-3">
+            <Text className="font-SpaceGrotesk text-xl text-white">Join And RSVP</Text>
+          </Pressable>
+        )}
       </View>
     </>
   );
